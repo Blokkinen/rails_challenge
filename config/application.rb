d4f651c8696dc0bb20ef93f805a7c3569fdb35ee
blokkinen <viktoria_blok@hotmail.com>
2016-09-05 15:26:45 +0200

    setup cucumber

--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,13 @@ Bundler.require(*Rails.groups)
 
 module RailsChallenge
   class Application < Rails::Application
-    config.generators do |generate|
-    generate.helper false
-    generate.assets false
-    generate.view_specs false
-    generate.helper_specs false
-    generate.routing_specs false
-    generate.controller_specs false
+      config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
   end
 end
